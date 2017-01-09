8704dbf9427827edf11997dd2786d2ff2b7edb70
micklethepickle <michel_ma@hotmail.ca>
2017-01-09 16:00:23 -0500

    deuced

--- public/scripts/textChanger.js
+++ public/scripts/textChanger.js
@@ -5,10 +5,10 @@ $(function(){
 })
 
 function changeTxt(){
-	$.each(['programmer', 'physicist', 'student', 'meme-economist'], function(index, value){
+	$.each(['student' , 'physicist', 'redditor', 'programmer'], function(index, value){
 		setTimeout(function(){
 			$('.secondPart').fadeOut(500, function(){
-				$('.secondPart').text(value).removeClass('meme-economist').removeClass('programmer').removeClass('physicist').removeClass('student').addClass(value);
+				$('.secondPart').text(value).removeClass('redditor').removeClass('programmer').removeClass('physicist').removeClass('student').addClass(value);
 			}).delay(200).fadeIn(500);
 		}, index * 2000);
 	});
